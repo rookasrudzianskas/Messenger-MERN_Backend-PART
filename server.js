d7ba8236a7edba53069beb76186fe0ed8041b427
Rokas <rookas.rudzenskas@gmail.com>
2021-06-14 14:30:41 +0300

    Rokas

--- server.js
+++ server.js
@@ -6,12 +6,23 @@ import cors from "cors";
 
 // app config
 const app = express();
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 8000;
 
 // middlewares
 
 
 // db config
+
+const mongoURI = 'mongodb+srv://admin:e1mte3hnzmomacf9@cluster0.etlot.mongodb.net/messenger-backend?retryWrites=true&w=majority';
+mongoose.connect(mongoURI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+mongoose.connection.once('open', () => {
+    console.log('DB is connected');
+})
 
 // api routes
 app.get('/', (req, res) => res.status(200).send("Hello World 🚀"));
